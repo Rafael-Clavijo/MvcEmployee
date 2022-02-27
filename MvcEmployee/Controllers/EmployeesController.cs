1920f65def6734136b984d083f6c08c74e8d15cd
Rafael Humberto Clavijo Ortiz <rafael.clavijo@ICA.GOV.CO>
2022-02-27 15:02:10 -0500

    Add project files.

---
+++ MvcEmployee/Controllers/EmployeesController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcEmployee.Models;
+
+namespace MvcEmployee.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeeDBContext db = new EmployeeDBContext();
+
+        // GET: Employees
+        public ActionResult Index(string searchString)
+        {
+            //Get all employees
+            var employees = (from e in db.Employees select e).ToList();
+            //Filter Employees
+            if (!String.IsNullOrEmpty(searchString)) employees = employees.Where(s => s.Id.ToString().Contains(searchString)).ToList();
+            //Fix calculated Employee properties
+            employees.ForEach(hire => hire.AnnualSalary = hire.Salary * 12);
+            //Display result
+            return View(employees);
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+                return HttpNotFound();
+            else
+                employee.AnnualSalary = employee.Salary * 12;
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Salary,Age,ProfileImage,AnnualSalary")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+                return HttpNotFound();
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Salary,Age,ProfileImage,AnnualSalary")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+                return HttpNotFound();
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcEmployee/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcEmployee.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public short Age { get; set; }
+        [Display(Name = "Profile Image")]
+        public byte[] ProfileImage { get; set; }
+        [Display(Name = "Annual Salary")]
+        public int AnnualSalary { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
